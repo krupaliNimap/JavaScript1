16d7b7f9975022e54e012da43117187cf867c8de
krupaliNimap <krupali@himapinfotech.com>
2023-08-23 12:30:32 +0530

    coercion

--- august23_23/Coercion.js
+++ august23_23/Coercion.js
@@ -3,9 +3,29 @@ let num2 = "9";
 let num3 = "upo";
 let num4 = 0;
 
+//Boolean coersion
+if (num1) {
+  console.log("num1", num1);
+} else {
+  console.log("num2", num2);
+}
+console.log("-------------------------------");
+if (num2) {
+  console.log("num2", num2);
+} else {
+  console.log("num1", num1);
+}
+console.log("-------------------------------");
+
+//Logical operator coercion
 console.log(`both truthy ${num2 || num3}`);
 console.log(`first falsy second truthy ${num1 || num3}`);
 console.log(`first truthy second falsy ${num3 || num1}`);
 console.log(`first falsy second falsy ${num4 || num1}`);
+console.log("-------------------------------");
 
-console.log(typeof typeof NaN);
+console.log(`both truthy ${num2 && num3}`);
+console.log(`first falsy second truthy ${num1 && num3}`);
+console.log(`first truthy second falsy ${num3 && num1}`);
+console.log(`first falsy second falsy ${num4 && num1}`);
+console.log("-------------------------------");
